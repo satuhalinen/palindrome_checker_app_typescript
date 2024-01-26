0cfa498b1dad43d314c19a81f06b1026fb54a58b
[satuhalinen] <satuh9@gmail.com>
2024-01-26 22:12:05 +0200

    add the functionality to tell the palindrome

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
-import './style.css'
+import "./style.css";
 
-function searchHandler(){
-  console.log("Test");
+function searchHandler() {
+  let text: string;
+  let input = (document.querySelector("#ask") as HTMLInputElement)?.value;
+  let emptyAway = input?.toLowerCase().replaceAll(" ", "");
+  let toArray = emptyAway?.split("");
+  let toReverse = toArray?.reverse();
+  let palindrome = toReverse?.join("");
+  if (input == "") {
+    return "";
+  } else if (emptyAway == palindrome) {
+    text = "The given word/number/phrase is a palindrome.";
+  } else {
+    text = "The given word/number/phrase is not a palindrome.";
   }
- 
-  let input = document.querySelector("#feed");
-  input?.addEventListener("input", searchHandler);
+  let result = document.querySelector("#result");
+  if (result) {
+    result.textContent = text;
+  }
+}
 
+let ask = document.querySelector("#ask") as HTMLInputElement;
+if (ask instanceof HTMLElement) {
+  ask.addEventListener("input", searchHandler);
+}
